7c4500e33d9321a44c728cf3d03eee14647f399b
tusharjana <165192660+tusharjana@users.noreply.github.com>
2024-04-10 01:19:27 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,11 +4,11 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = "info.delhi@nift.ac.in";
+$email_from = "";
 $email_subject = "New Form Submission";
 $email_body = "User : $name.\n"."User Email : $visitor_email.\n"."Subject : $subject.\n"."Message : $message.\n";
 
-$to = "tushar.jana@bcah.christuniversity.in";
+$to = "";
 $headers = "From : $email_from \r\n";
 $headers .= "Reply-To : $visitor_email \r\n";
 
